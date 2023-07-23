f34c4b573fde0a81140bbcd99865f0dad64dad6b
heiblack850930 <heiblack850930@gmail.com>
2023-07-23 16:41:05 +0800

    錯誤更新，符合Wordpress 規範

--- rcm-external-link-message-page.php
+++ rcm-external-link-message-page.php
@@ -15,7 +15,8 @@
 
 use rcm_eump_transparencyreport\WP_Customize_Tiny_Control;
 
-class RcmExternalLinkMessagePage{
+class RcmExternalLinkMessagePage
+{
 
     public function __construct()
     {
@@ -28,9 +29,8 @@ class RcmExternalLinkMessagePage{
     private function init()
     {
 
-        register_activation_hook(__FILE__, function ()
-        {
-            $page_title = __('RCM External Link Message Page','rcm-external-link-message-page');
+        register_activation_hook(__FILE__, function () {
+            $page_title = __('RCM External Link Message Page', 'rcm-external-link-message-page');
             $page_content = '';
             $page_check = get_page_by_title($page_title);
             $new_page = array(
@@ -40,7 +40,7 @@ class RcmExternalLinkMessagePage{
                 'post_status' => 'publish',
                 'post_author' => 1,
             );
-            if(!isset($page_check->ID)) {
+            if (!isset($page_check->ID)) {
                 $new_page_id = wp_insert_post($new_page);
                 update_post_meta($new_page_id, '_wp_page_template', 'templates/rottencodemonkey-eump.php');
                 //update option
@@ -48,19 +48,18 @@ class RcmExternalLinkMessagePage{
             }
         });
 
-        add_action( 'wp_enqueue_scripts',  function () {
-            if ( is_page_template( 'templates/rottencodemonkey-eump.php' ) ) {
-                wp_enqueue_style( 'rottencodemonkey-eump', plugin_dir_url(__FILE__) .  'assets/css/rottencodemonkey-eump.css' );
-                $rcm_eump_page_css = get_theme_mod('rcm_eump_css');
-                wp_register_style( 'rcm_eump_page_css', false );
-                wp_enqueue_style( 'rcm_eump_page_css' );
-                wp_add_inline_style( 'rcm_eump_page_css', $rcm_eump_page_css);
-
+        add_action('wp_enqueue_scripts',  function () {
+            if (is_page_template('templates/rottencodemonkey-eump.php')) {
+                wp_enqueue_style('rottencodemonkey-eump', plugin_dir_url(__FILE__) .  'assets/css/rottencodemonkey-eump.css');
+                $rcm_eump_page_css = get_option('rcm_eump_css_setting');
+                wp_register_style('rcm_eump_page_css', false);
+                wp_enqueue_style('rcm_eump_page_css');
+                wp_add_inline_style('rcm_eump_page_css', $rcm_eump_page_css);
             }
         });
 
-        add_action('wp_ajax_rcm_get_eump', function (){
-            if (is_admin()){
+        add_action('wp_ajax_rcm_get_eump', function () {
+            if (is_admin()) {
                 $page_id = get_option('rcm_eump_page_id');
                 //page id to get url
                 $page_url = get_permalink($page_id);
@@ -68,11 +67,11 @@ class RcmExternalLinkMessagePage{
                 die();
             }
             die();
-        } );
+        });
 
         //allow use iframe tag
-        add_filter( 'wp_kses_allowed_html', function ( $tags, $context ) {
-            if ( 'post' === $context ) {
+        add_filter('wp_kses_allowed_html', function ($tags, $context) {
+            if ('post' === $context) {
                 $tags['iframe'] = array(
                     'src'             => true,
                     'height'          => true,
@@ -82,11 +81,11 @@ class RcmExternalLinkMessagePage{
                 );
             }
             return $tags;
-        }, 10, 2 );
+        }, 10, 2);
 
 
-        add_action('customize_controls_enqueue_scripts',function(){
-            wp_enqueue_script( 'wp-rcm-external-link-message-page', plugin_dir_url(__FILE__) . '/assets/js/rcm-external-link-message-page.js');
+        add_action('customize_controls_enqueue_scripts', function () {
+            wp_enqueue_script('wp-rcm-external-link-message-page', plugin_dir_url(__FILE__) . '/assets/js/rcm-external-link-message-page.js');
         });
 
 
@@ -96,130 +95,176 @@ class RcmExternalLinkMessagePage{
     }
     private function rcmCreateTemplates()
     {
-        add_filter( 'page_template',  function ( $page_template ) {
-            if ( get_page_template_slug() == 'templates/rottencodemonkey-eump.php' ) {
+        add_filter('page_template',  function ($page_template) {
+            if (get_page_template_slug() == 'templates/rottencodemonkey-eump.php') {
                 $page_template = dirname(__FILE__) . '/templates/rottencodemonkey-eump.php';
             }
             return $page_template;
-        },10, 1 );
-        add_filter( 'theme_page_templates',  function ( $post_templates, $wp_theme, $post, $post_type ) {
-            $post_templates['templates/rottencodemonkey-eump.php'] = __( 'RCM EUMP', 'rcm-external-link-message-page' );
+        }, 10, 1);
+        add_filter('theme_page_templates',  function ($post_templates, $wp_theme, $post, $post_type) {
+            $post_templates['templates/rottencodemonkey-eump.php'] = __('RCM EUMP', 'rcm-external-link-message-page');
             return $post_templates;
-        }, 10, 4 );
+        }, 10, 4);
     }
     private function rcmCreateWpCustomize()
     {
-        add_action( 'customize_register', function ($wp_customize){
+        add_action('customize_register', function ($wp_customize) {
 
-            require  plugin_dir_path( __FILE__ ) . 'inc/Customize-TinyMCE.php';
+            require  plugin_dir_path(__FILE__) . 'inc/Customize-TinyMCE.php';
 
             $wp_customize->add_section(
-                'rcm-external-link-message-page',
+                'rcm_external_link_message_page_section',
                 array(
-                    'title' => __('RCM External Link Message Page','rcm-external-link-message-page'),
+                    'title' => __('RCM External Link Message Page2', 'rcm-external-link-message-page'),
+                    'priority' => 30,
+
                 )
             );
-            $wp_customize->add_setting('rcm_eump_title',array(
-                'default' => __('You Are Leaving...','rcm-external-link-message-page'),
+            $wp_customize->add_setting('rcm_eump_title_setting', array(
+                'default' => __('You Are Leaving...', 'rcm-external-link-message-page'),
+                'sanitize_callback' => 'sanitize_text_field',
+                'capability'        => 'edit_theme_options',
+                'type'              => 'option',
 
             ));
-            $wp_customize->add_control('rcm_eump_title',array(
-                'label'=> __('Title','rcm-external-link-message-page'),
-                'type'=>'text',
-                'section'=>'rcm-external-link-message-page',
+            $wp_customize->add_control('rcm_eump_title_setting', array(
+                'label' => __('Title', 'rcm-external-link-message-page'),
+                'type' => 'text',
+                'section' => 'rcm_external_link_message_page_section',
+                'settings' => 'rcm_eump_title_setting',
             ));
-            $wp_customize->add_setting('rcm_eump_header',array(
-                'transport' => 'refresh',
+
+            $wp_customize->add_setting('rcm_eump_header_setting', array(
+                'default'           => '',
+                'sanitize_callback' => 'sanitize_text_field',
+                'capability'        => 'edit_theme_options',
+                'type'              => 'option',
             ));
-            $wp_customize->add_control('rcm_eump_header',
+            $wp_customize->add_control(
+                'rcm_eump_heade_setting',
                 array(
                     'type' => 'checkbox',
-                    'label' => __('Show Header','rcm-external-link-message-page'),
-                    'section' => 'rcm-external-link-message-page',
+                    'label' => __('Show Header', 'rcm-external-link-message-page'),
+                    'settings' => 'rcm_eump_header_setting',
+                    'section' => 'rcm_external_link_message_page_section',
                 )
             );
-            $wp_customize->add_setting( 'rcm_eump_logo', array(
-                'transport' => 'refresh',
+            $wp_customize->add_setting( 'rcm_eump_logo_setting', array(
+                'default'           => '',
+                'sanitize_callback' => 'sanitize_text_field',
+                'capability'        => 'edit_theme_options',
+                'type'              => 'option',
             ));
             $wp_customize->add_control( new WP_Customize_Image_Control( $wp_customize, 'rcm_eump_logo', array(
                 'label' => __('Site Logo','rcm-external-link-message-page'),
-                'section' => 'rcm-external-link-message-page',
-                'settings' => 'rcm_eump_logo',
+                'section' => 'rcm_external_link_message_page_section',
+                'settings' => 'rcm_eump_logo_setting',
             )));
-            $wp_customize->add_setting('rcm_eump_text_1', array(
-                'transport' => 'refresh',
+            $wp_customize->add_setting('rcm_eump_text_1_setting', array(
+                'default'           => '',
+                'sanitize_callback' => 'wp_kses_post',
+                'capability'        => 'edit_theme_options',
+                'type'              => 'option',
             ));
             $wp_customize->add_control(new WP_Customize_Tiny_Control( $wp_customize, 'rcm_eump_text_1', array(
-                'section' => 'rcm-external-link-message-page',
+                'section' => 'rcm_external_link_message_page_section',
+                'settings' => 'rcm_eump_text_1_setting',
                 'label'=>'Body',
             )));
-            $wp_customize->add_setting('rcm_eump_transparencyreport', array(
-                'transport' => 'refresh',
+            $wp_customize->add_setting('rcm_eump_transparencyreport_setting', array(
+                'default'           => '',
+                'sanitize_callback' => 'sanitize_text_field',
+                'capability'        => 'edit_theme_options',
+                'type'              => 'option',
             ));
-            $wp_customize->add_control('rcm_eump_transparencyreport',
+            $wp_customize->add_control('rcm_eump_transparencyreport_setting',
                 array(
                     'type' => 'checkbox',
                     'label' => __( 'Show Google Transparency Report', 'rcm-external-link-message-page' ),
-                    'section' => 'rcm-external-link-message-page',
+                    'section' => 'rcm_external_link_message_page_section',
+                    'settings' => 'rcm_eump_transparencyreport_setting',
                 )
             );
-            $wp_customize->add_setting('rcm_eump_text5',array(
+            $wp_customize->add_setting('rcm_eump_text5_setting',array(
                 'default' => __( 'Find the site I\'m going to', 'rcm-external-link-message-page' ),
-                'transport' => 'refresh',
+                'sanitize_callback' => 'sanitize_text_field',
+                'capability'        => 'edit_theme_options',
+                'type'              => 'option',
             ));
-            $wp_customize->add_control('rcm_eump_text5',array(
+            $wp_customize->add_control('rcm_eump_text5_setting',array(
                 'label'=> __( 'Google Transparency Report', 'rcm-external-link-message-page' ),
                 'type'=>'text',
-                'section'=>'rcm-external-link-message-page',
+                'section'=>'rcm_external_link_message_page_section',
+                'settings' => 'rcm_eump_text5_setting',
             ));
-            $wp_customize->add_setting('rcm_eump_text6',array(
+            $wp_customize->add_setting('rcm_eump_text6_setting',array(
                 'default' => __( 'Confirm to go', 'rcm-external-link-message-page' ),
-                'transport' => 'refresh',
+                'sanitize_callback' => 'sanitize_text_field',
+                'capability'        => 'edit_theme_options',
+                'type'              => 'option',
 
             ));
-            $wp_customize->add_control('rcm_eump_text6',array(
+            $wp_customize->add_control('rcm_eump_text6_setting',array(
                 'label'=> __( 'Button Text', 'rcm-external-link-message-page' ),
                 'type'=>'text',
-                'section'=>'rcm-external-link-message-page',
+                'section'=>'rcm_external_link_message_page_section',
+                'settings' => 'rcm_eump_text6_setting',
             ));
-            $wp_customize->add_setting('rcm_eump_text_2', array(
-                'transport' => 'refresh',
+
+            $wp_customize->add_setting('rcm_eump_text_2_setting', array(
+                'default'           => '',
+                'sanitize_callback' => 'wp_kses_post',
+                'capability'        => 'edit_theme_options',
+                'type'              => 'option',
             ));
-            $wp_customize->add_control(new WP_Customize_Tiny_Control( $wp_customize, 'rcm_eump_text_2', array(
+            $wp_customize->add_control(new WP_Customize_Tiny_Control( $wp_customize, 'rcm_eump_text_2_setting', array(
                 'label'=> __( 'Center Text', 'rcm-external-link-message-page' ),
-                'section' => 'rcm-external-link-message-page',
+                'section' => 'rcm_external_link_message_page_section',
+                'settings' => 'rcm_eump_text_2_setting',
             )));
-            $wp_customize->add_setting('rcm_eump_text_3', array(
-                'transport' => 'refresh',
+            $wp_customize->add_setting('rcm_eump_text_3_setting', array(
+                'default'           => '',
+                'sanitize_callback' => 'wp_kses_post',
+                'capability'        => 'edit_theme_options',
+                'type'              => 'option',
             ));
-            $wp_customize->add_control(new WP_Customize_Tiny_Control( $wp_customize, 'rcm_eump_text_3', array(
+            $wp_customize->add_control(new WP_Customize_Tiny_Control( $wp_customize, 'rcm_eump_text_3_setting', array(
                 'label'=> __( 'Footer Text', 'rcm-external-link-message-page' ),
-                'section' => 'rcm-external-link-message-page',
+                'section' => 'rcm_external_link_message_page_section',
+                'settings' => 'rcm_eump_text_3_setting',
             )));
 
-            $wp_customize->add_setting('rcm_eump_poweredby', array(
-                'transport' => 'refresh',
+            $wp_customize->add_setting('rcm_eump_poweredby_setting', array(
+                'default'           => '',
+                'sanitize_callback' => 'sanitize_text_field',
+                'capability'        => 'edit_theme_options',
+                'type'              => 'option',
             ));
-            $wp_customize->add_control('rcm_eump_poweredby',
+            $wp_customize->add_control('rcm_eump_poweredby_setting',
                 array(
                     'type' => 'checkbox',
                     'label' => __( 'Hide Copyright Notice', 'rcm-external-link-message-page' ),
-                    'section' => 'rcm-external-link-message-page',
+                    'section' => 'rcm_external_link_message_page_section',
                     'description' => __( '', 'rcm-external-link-message-page' ),
+                    'settings' => 'rcm_eump_poweredby_setting',
                 )
             );
-            $wp_customize->add_setting( 'rcm_eump_css', array(
-                'transport' => 'refresh',
+            $wp_customize->add_setting( 'rcm_eump_css_setting', array(
+                'default'           => '',
+                'sanitize_callback' => 'wp_kses_post',
+                'capability'        => 'edit_theme_options',
+                'type'              => 'option',
             ));
-            $wp_customize->add_control( new WP_Customize_Code_Editor_Control( $wp_customize, 'rcm_eump_css', array(
+
+            $wp_customize->add_control( new WP_Customize_Code_Editor_Control( $wp_customize, 'rcm_eump_css_setting', array(
                 'label' => __( 'Customize CSS', 'rcm-external-link-message-page' ),
-                'section' => 'rcm-external-link-message-page',
-                'settings' => 'rcm_eump_css',
+                'section' => 'rcm_external_link_message_page_section',
+                'settings' => 'rcm_eump_css_setting',
                 'code_type' => 'text/css',
             )));
 
 
-        } );
+        });
     }
 }
 
@@ -227,3 +272,6 @@ class RcmExternalLinkMessagePage{
 
 
 new RcmExternalLinkMessagePage();
+
+
+
--- templates/rottencodemonkey-eump.php
+++ templates/rottencodemonkey-eump.php
@@ -5,42 +5,42 @@
     <meta charset="<?php bloginfo( 'charset' ); ?>" />
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <title><?php esc_html_e(get_theme_mod('rcm_eump_title'));?></title>
+    <title><?php esc_html_e(get_option('rcm_eump_title_setting'));?></title>
     <?php wp_head(); ?>
 
 </head>
 <body <?php body_class(); ?>>
 <!--body-->
-<?php if(get_theme_mod('rcm_eump_header') == 1): ?>
+<?php if(get_option('rcm_eump_header_setting') == 1): ?>
     <div class="rcm-header">
-        <img class="logo" src="<?php esc_html_e(get_theme_mod('rcm_eump_logo'));?>" alt="">
+        <img class="logo" src="<?php esc_html_e(get_option('rcm_eump_logo_setting'));?>" alt="">
     </div>
 <?php endif; ?>
 <div class="rcm-body-container">
-    <?php echo wp_kses_post(get_theme_mod('rcm_eump_text_1')); ?>
+    <?php echo wp_kses_post(get_option('rcm_eump_text_1_setting')); ?>
 </div>
 <!--transparencyreport.google.com-->
-<?php if(get_theme_mod('rcm_eump_transparencyreport') == 1 && $url = esc_url(isset($_GET['url']))): ?>
+<?php if(get_option('rcm_eump_transparencyreport_setting') == 1 && $url = esc_url(isset($_GET['url']))): ?>
     <div class="rcm-transparencyreport">
-        <a href="https://transparencyreport.google.com/safe-browsing/search?url=<?php echo esc_url($url) ?>"><?php echo esc_html_e(get_theme_mod('rcm_eump_text5')); ?></a>
+        <a href="https://transparencyreport.google.com/safe-browsing/search?url=<?php echo esc_url($url) ?>"><?php echo esc_html_e(get_option('rcm_eump_text5_setting')); ?></a>
     </div>
 <?php endif; ?>
 <!--RCM Center-->
 <div class="rcm-center">
-    <?php echo wp_kses_post(get_theme_mod('rcm_eump_text_2')); ?>
+    <?php echo wp_kses_post(get_option('rcm_eump_text_2_setting')); ?>
 </div>
 
 <!--go to url-->
 <div class="rcm-button-container">
-    <?php if($url = esc_url(isset($_GET['url']))): ?>
-    <a href="<?php echo esc_url($url) ?>"><button class="rcm-button" role="button"><?php esc_html_e(get_theme_mod('rcm_eump_text6')); ?></button></a>
+    <?php if($url = esc_url(@$_GET['url'])): ?>
+    <a href="<?php echo esc_url($url) ?>"><button class="rcm-button" role="button"><?php esc_html_e(get_option('rcm_eump_text6_setting')); ?></button></a>
     <?php endif; ?>
 </div>
 <!--RCM Footer-->
 <div class="rcm-footer">
-    <?php echo wp_kses_post(get_theme_mod('rcm_eump_text_3')); ?>
+    <?php echo wp_kses_post(get_option('rcm_eump_text_3_setting')); ?>
 </div>
-<?php if(get_theme_mod('rcm_eump_poweredby') != 1 ): ?>
+<?php if(get_option('rcm_eump_poweredby_setting') != 1 ): ?>
     <div id="rottenCodeMonkey">
         Powered by RottenCodeMonkey
     </div>
